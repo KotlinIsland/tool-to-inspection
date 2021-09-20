13d9ad5ac738414c3d94d9154624aed685265024
GitHub Action <action@github.com>
2021-09-20 08:30:29 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/kotlinisland/tooltoinspection/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.kotlinisland.tooltoinspection.services
+
+import com.github.kotlinisland.tooltoinspection.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
